bc49b4f83da7dc7a5b30cc95316cce34936f50ef
Sage Ross <sage@ragesoss.com>
2017-06-26 16:56:32 -0700

    Avoid destructing potentially-large arrays with splatted argument
    This should head off any similar stack too deep errors like #1314.

--- lib/utils.rb
+++ lib/utils.rb
@@ -6,9 +6,8 @@ class Utils
   def self.chunk_requests(items, chunk_size=50)
     blocks = items.each_slice(chunk_size).to_a
     results = []
-    blocks.each do |b|
-      info = yield b
-      results.push(*info)
+    blocks.each do |block|
+      results += yield block
     end
     results
   end
